dfd0e50224f8aaad2c86edad1004428a73b47fec
David Sepulveda <dsepulvedara@gmail.com>
2019-01-31 23:20:48 -0500

    Updating Example to publish to Ubidots broker

--- examples/UbidotsPublish/UbidotsPublish.ino
+++ examples/UbidotsPublish/UbidotsPublish.ino
@@ -9,28 +9,12 @@
  ****************************************/
 
 #ifndef TOKEN
-#define TOKEN "Your TOKEN"  // Add here your Ubidots TOKEN
+#define TOKEN "Your_TOKEN"  // Put here your Ubidots TOKEN
 #endif
 
+#define VARIABLE_LABEL "YOUR_VARIABLE_LABEL"
 
-/****************************************
- * Auxiliar Functions
- ****************************************/
-
-void callback(char* topic, byte* payload, unsigned int length) {
-    Serial.print("Message arrived [");
-    Serial.print(topic);
-    Serial.print("] ");
-    Serial.println("payload obtained from server:");
-    for (int i=0;i<length;i++) {
-        Serial.print((char)payload[i]); // prints the answer of the broker for debug purpose
-    }
-    // Some stuff to make with the payload obtained
-        //
-   //
-    Serial.println();
-}
-
+void callback(char* topic, byte* payload, unsigned int length); // callback most be defined
 
 /****************************************
  * Instances
@@ -39,29 +23,39 @@ void callback(char* topic, byte* payload, unsigned int length) {
 Ubidots client(TOKEN, callback);
 
 /****************************************
+ * Auxiliar Functions
+ ****************************************/
+
+// Put here your auxiliar functions.
+
+/****************************************
  * Main Functions
  ****************************************/
 
 void setup() {
     Serial.begin(115200);
-    client.initialize();
 
-    // Uncomment this line if you have a business Ubidots account
-    //client.ubidotsSetBroker("business.api.ubidots.com");
+    // Comment below line to disable debug messages in the serial port.
+    client.ubidotsSetDebug(true);
+
+    // Uncomment this line if you have an Ubidots for Education account
+    //client.ubidotsSetBroker("things.ubidots.com");
+
+    // Connects to Ubidot Ubidots' Broker
+    client.connect(5);
+
 }
 
 void loop() {
+
     if(!client.isConnected()){
-        client.reconnect();
+        client.connect(5);
     }
 
     // Publish routine, if the device and variables are not created they will be created
-    float value = 1;
-    Serial.println("Sending value");
-    client.add("test-var-1", value); // Insert as first parameter your variable label
-    client.add("test-var-2", value, "\"lat\":10.302, \"lng\":2.9384"); //Adds value with context
-    client.add("test-var-3", value, NULL, 1492445109); // Adds value with custom timestamp
-    client.ubidotsPublish("test-device"); // Insert your device label where the values will be stored in Ubidots
+    float value = analogRead(A0);
+    client.add(VARIABLE_LABEL, value); // Insert as first parameter your variable label
+    client.ubidotsPublish();
 
     // Client loop for publishing and to maintain the connection
     client.loop();
